02b675b5223371e14294243fff638e4dd1839e44
Tort5i <96796556+Tort5i@users.noreply.github.com>
2025-08-17 15:27:29 +0100

    created Chip-8 base system

---
+++ source/Chip-8.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+class Chip8 {
+private:
+    unsigned short opcode;
+    unsigned char memory[4096];
+    unsigned char V[16];
+    unsigned short I;
+    unsigned short pc;
+    unsigned char gfx[64 * 32];
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+    unsigned short stack[16];
+    unsigned short stackPointer;
+    unsigned char key[16];
+
+public:
+    void initilize();
+    void emulateCycle();
+};
